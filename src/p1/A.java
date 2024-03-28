2c93eff7005742d401a1b64f40b268c2a91a1f7b
Himanshu Kumar Singh <himanshukumarsingh521@gmail.com>
2024-03-28 23:42:05 +0530

    commit 3

--- src/p1/A.java
+++ src/p1/A.java
@@ -1,5 +1,15 @@
 package p1;
 
-public class A {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-}
+public class A{
+		public static void main(String[]args) {
+			List<Integer>data= Arrays.asList(12,34,55);
+			List<Integer>newDta=data.stream().map(e->e*2).collect(Collectors.toList());
+			System.out.println(newDta);
+		}
+		}
+
+
